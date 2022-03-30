8d5ae3bca22e4b4ed7b8d4d77c5eec3769c9355b
orlipora <orlipora@gmail.com>
2022-03-30 20:14:50 -0300

    primer programa

---
+++ semana_6/contr_temp_horno.py
@@ -0,0 +1,39 @@
+
+import random
+import  os
+
+
+temp_test = int(input('Ingrese temperatura de testeo :' ))
+
+
+while temp_test != 0:
+    os.system('cls')
+    lis_temp = []
+    suma1 = 0
+    for i in range(50):
+        lis_temp.append (random.randint(300,320))
+        suma1 = lis_temp[i] +suma1
+        lis_temp.sort()
+        
+    suma = suma1 /50
+    print('Temp_Promedio : ', suma, 'ºC')
+    print('tem_test : ', temp_test , 'ºC')
+    print(lis_temp)
+    
+    temp_ref =  suma - temp_test  
+    print('Temp de referencia : ', temp_ref , 'ºC')
+
+    if temp_ref < 200:
+        print('Encender 2 Quemadores')
+
+    elif  temp_ref >= 200 and  temp_ref <300:
+        print('Encender 1 Quemadores')
+    elif  temp_ref >= 300  and temp_ref < 320 :
+        print('Temperatura Correcta')
+    else:
+        print('Fuera de rango')
+
+    temp_test = int(input('Ingrese nueva temperatura de testeo :' ))
+    
+else:
+    print('FIN')
