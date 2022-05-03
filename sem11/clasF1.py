c8027e51dc41ce8499d0cda35648f0d96a121567
orlipora <orlipora@gmail.com>
2022-05-02 22:00:45 -0300

    corregido

--- sem11/clasF1.py
+++ sem11/clasF1.py
@@ -2,6 +2,8 @@
 import json
 import os
 RUTA = 'sem11/form1.json'
+lis = []
+
 os.system('cls')
 
 def leer_arch (RUTA):
@@ -12,10 +14,21 @@ def leer_arch (RUTA):
 
 def main():
     ARCHIVO = leer_arch (RUTA)
-    print(["MRData"]["StandingsTable"]["StandingsLists"][0]["DriverStandings"][0]["points"])
+
+    for i in range(3):
+        puntos = ARCHIVO["MRData"]["StandingsTable"]["StandingsLists"][0]["DriverStandings"][i]["points"]
+        victoria = ARCHIVO["MRData"]["StandingsTable"]["StandingsLists"][0]["DriverStandings"][i]["wins"]
+
+        nombre = ARCHIVO["MRData"]["StandingsTable"]["StandingsLists"][0]["DriverStandings"][i]["Driver"]["givenName"]
+        apellido = ARCHIVO["MRData"]["StandingsTable"]["StandingsLists"][0]["DriverStandings"][i]["Driver"]["familyName"]
+        datos = f'El corredor  {nombre} {apellido}  tiene  {victoria}  victorias y acumulo  {puntos}  puntos .'
+        lis.append(datos)
+    return lis
 
 
 
 if __name__ == '__main__':
      main()
+     for i in lis:
+         print(i )
      
